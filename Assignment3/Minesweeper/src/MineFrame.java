37d9b2a11bbb1ce981421b7111d031fcfbe98427
Kyle Riedemann <kylealanr@gmail.com>
2015-02-25 08:30:01 -0600

    now creating Game object based on JOption panel.

--- Assignment3/Minesweeper/src/MineFrame.java
+++ Assignment3/Minesweeper/src/MineFrame.java
@@ -6,23 +6,24 @@ import java.awt.*;
  */
 public class MineFrame extends JFrame {
     public final String TITLE = "Minesweeper";
-    public final int START_HEIGHT = 100;
-    public final int START_WIDTH = 400;
     public final int HEIGHT = 800;
     public final int WIDTH = 800;
     ScorePanel scorePanel;
     MinePanel minePanel;
+    Game game;
+
+    //difficulty constants
+    public static final int EASY = 1;
+    public static final int MEDIUM = 2;
+    public static final int HARD = 3;
+    public static final int BOMBASTIC = 4;
 
     MineFrame(){
-        setSize(START_WIDTH, START_HEIGHT);
         setTitle(TITLE);
+        setSize(WIDTH, HEIGHT);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         Container container = getContentPane();
-
-
-
-        //TODO JOptions panel to select the difficulty
 
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         Image im = toolkit.getImage("assets/simple_bomb.png");
@@ -34,37 +35,35 @@ public class MineFrame extends JFrame {
 
         int difficlty = JOptionPane.showOptionDialog(null, "Please pick a difficulty", "Minesweeper", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, imageIcon, setValue, setValue[1]);
 
-        //TODO construct game object with specified difficulty here
         switch (difficlty){
             case 0:
                 System.out.println("EASY");
+                game = new Game(EASY);
                 break;
             case 1:
                 System.out.println("MEDIUM");
+                game = new Game(MEDIUM);
                 break;
             case 2:
                 System.out.println("HARD");
+                game = new Game(HARD);
                 break;
             case 3:
                 System.out.println("BOMBASTIC");
+                game = new Game(BOMBASTIC);
                 break;
             default:
                 System.out.println("EASY");
+                game = new Game(EASY);
                 break;
         }
-
-        //TODO pass the game we make here to these constructors
-        /*
-        container.remove(difficultyPanel);
-
-        setSize(WIDTH, HEIGHT);
 
         minePanel = new MinePanel(game);
         scorePanel = new ScorePanel(game);
 
         container.add(scorePanel, BorderLayout.NORTH);
         container.add(minePanel, BorderLayout.CENTER);
-        */
+
         setVisible(true);
     }
 
