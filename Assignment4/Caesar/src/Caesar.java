a75cd8db2ae89d4d32d68b28009254efe93ba618
Kyle Riedemann <kylealanr@gmail.com>
2015-03-03 17:03:17 -0600

    added in.txt with lorem ipsum and handled program being run with the wrong number of arguments

--- Assignment4/Caesar/src/Caesar.java
+++ Assignment4/Caesar/src/Caesar.java
@@ -8,8 +8,19 @@ import java.io.File;
 public class Caesar {
     File inFile;
     File outFile;
+    int key;
+
+    public Caesar(File inFile, File outFile, int key){
+        this.inFile = inFile;
+        this.outFile = outFile;
+        this.key = key;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        if(args.length != 3)
+        {
+            System.out.println("Proper Usage is: java Caesar key infile [outfile]");
+            System.exit(0);
+        }
     }
 }
---
+++ Assignment4/Caesar/src/Vigenere.java
@@ -0,0 +1,14 @@
+import java.io.File;
+
+/**
+ * Created by kyle on 3/3/15.
+ */
+public class Vigenere {
+    File inFile;
+    File outFile;
+    int key;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
