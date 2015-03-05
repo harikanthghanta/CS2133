d57e54b32416a0647a8ed0e0c66c3850a67b9ae2
Kyle Riedemann <kylealanr@gmail.com>
2015-03-05 11:42:45 -0600

    wrote test method headers

--- Assignment4/Caesar/src/CaesarTest.java
+++ Assignment4/Caesar/src/CaesarTest.java
@@ -2,4 +2,26 @@
  * Created by kyle on 3/3/15.
  */
 public class CaesarTest {
+    public static void decryptValueTest(){
+
+    }
+
+    public static void noInputTest(){
+
+    }
+
+    public static void fileNotFoundTest(){
+
+    }
+
+    public static void filePermissionTest(){
+
+    }
+
+    public static void main(String[] args){
+        decryptValueTest();
+        noInputTest();
+        fileNotFoundTest();
+        filePermissionTest();
+    }
 }
