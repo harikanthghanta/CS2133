d65d38855d6528fe4dcc1f481340a615f6537dd4
Kyle Riedemann <kylealanr@gmail.com>
2015-03-05 11:39:20 -0600

    basic outline of program done. need to get input from user and handle a few use cases.

--- Assignment4/Caesar/src/Caesar.java
+++ Assignment4/Caesar/src/Caesar.java
@@ -1,8 +1,6 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FilePermission;
+import java.io.*;
 import java.security.AccessControlException;
-import java.security.AccessController;
+import java.util.Scanner;
 
 /**
  * Created by kyle on 3/3/15.
@@ -20,15 +18,94 @@ public class Caesar {
         this.key = key;
     }
 
+    public Caesar(){
+        this.inFile = new File("/home/kyle/JavaProjects/CS2133/Assignment4/Caesar/src/in.txt");
+        this.outFile = new File("/home/kyle/JavaProjects/CS2133/Assignment4/Caesar/src/out.txt");
+        this.key = 3;
+    }
+
+    public void caesarShift(){
+        try{
+            //Runtime.getRuntime().exec("chmod 777 " + "/home/kyle/JavaProjects/CS2133/Assignment4/Caesar/src/out.txt");
+
+            //FilePermission outPermission = new FilePermission("/home/kyle/JavaProjects/CS2133/Assignment4/Caesar/src/out.txt", "write");
+
+            //AccessController.checkPermission(outPermission);
+
+            Scanner scanner = new Scanner(inFile);
+            PrintWriter printWriter = new PrintWriter(outFile);
+
+
+            while (scanner.hasNextLine()){
+                String tempIn = scanner.nextLine();
+                String tempEncrypt = encryptLine(tempIn, key);
+                printWriter.write(tempEncrypt + "\n");
+            }
+            printWriter.flush();
+            printWriter.close();
+
+        } catch (FileNotFoundException e){
+            System.out.println("file not found");
+        } catch (AccessControlException e){
+            System.out.println("insufficient permissions");
+        } catch (IOException e){
+            System.out.println("could not set permissions");
+        }
+    }
+
+    public String encryptLine(String inLine, int key){
+        String outLine = "";
+
+        for(int i = 0; i < inLine.length(); i++){
+            char tempChar = inLine.charAt(i);
+            if (tempChar > 32 || tempChar < 126) tempChar = (char)tempChar;
+            tempChar = (char)(tempChar + key);
+            if (tempChar > 126) tempChar = (char)(tempChar - 95);
+            if (tempChar < 32) tempChar = (char)(tempChar  + 95);
+            outLine += tempChar;
+        }
+
+        return outLine;
+    }
+
+    public String decryptLine(String inLine, int key){
+        String outLine = "";
+
+        for(int i = 0; i < inLine.length(); i++){
+            char tempChar = inLine.charAt(i);
+            if (tempChar > 32 || tempChar < 126) tempChar = (char)tempChar;
+            tempChar = (char)(tempChar - key);
+            if (tempChar > 126) tempChar = (char)(tempChar - 95);
+            if (tempChar < 32) tempChar = (char)(tempChar  + 95);
+            outLine += tempChar;
+        }
+
+        return outLine;
+    }
+
     public static void main(String[] args) {
+        Caesar caesar = new Caesar();
+        caesar.caesarShift();
+
+        /*
+        File inFile;
+        File outFile;
+        int key;
+
         if(args.length != 3)
         {
             System.out.println("Proper Usage is: java Caesar key infile [outfile]");
             System.exit(0);
         }
+
+        //outFile = new File(args[2]);
+        outFile = new File("/home/kyle/JavaProjects/CS2133/Assignment4/Caesar/src/out.txt");
+        key = Integer.parseInt(args[0]);
+
         try {
-            File inFile = new File(args[1]);
-            if(!inFile.exists() || inFile.isDirectory()){
+            //inFile = new File(args[1]);
+            inFile = new File("/home/kyle/JavaProjects/CS2133/Assignment4/Caesar/src/in.txt");
+            if(!inFile.exists() && !inFile.isDirectory()){
                 throw new FileNotFoundException();
             }
 
@@ -38,11 +115,14 @@ public class Caesar {
 
             AccessController.checkPermission(inPermission);
             AccessController.checkPermission(outPermission);
+
+            Caesar caesar = new Caesar(inFile, outFile, key);
+            caesar.caesarShift();
         } catch (AccessControlException e){
             System.out.println("improper permissions for input or output file");
         } catch (FileNotFoundException e){
             System.out.println("input file doesn't exist");
         }
-
+        */
     }
 }
