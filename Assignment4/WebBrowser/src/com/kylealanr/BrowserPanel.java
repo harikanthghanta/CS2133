f0d5d246bdd6a3d2f9931b1de819c2107e5f9700
Kyle Riedemann <kylealanr@gmail.com>
2015-03-15 20:00:40 -0500

    added action listener on text field and made class that will handle the rendering

--- Assignment4/WebBrowser/src/com/kylealanr/BrowserPanel.java
+++ Assignment4/WebBrowser/src/com/kylealanr/BrowserPanel.java
@@ -2,6 +2,8 @@ package com.kylealanr;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by kyle on 3/15/15.
@@ -9,6 +11,12 @@ import java.awt.*;
 public class BrowserPanel extends JPanel {
     BrowserPanel(){
         JTextField textField = new JTextField();
+        textField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("event");
+            }
+        });
         setLayout(new BorderLayout());
         add(textField);
     }
---
+++ Assignment4/WebBrowser/src/com/kylealanr/BrowserRender.java
@@ -0,0 +1,7 @@
+package com.kylealanr;
+
+/**
+ * Created by kyle on 3/15/15.
+ */
+public class BrowserRender {
+}
