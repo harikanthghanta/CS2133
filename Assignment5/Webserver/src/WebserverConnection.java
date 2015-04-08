fc883733c51af77880d0ad9a1dec2e8539565fc8
Kyle Riedemann <kylealanr@gmail.com>
2015-04-07 20:16:41 -0500

    webserver works for the most part

--- Assignment5/Webserver/src/WebserverConnection.java
+++ Assignment5/Webserver/src/WebserverConnection.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Socket;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -10,15 +7,20 @@ import java.util.regex.Pattern;
  * Created by kyle on 4/7/15.
  */
 public class WebserverConnection implements Runnable {
-    public static final String INDEX_HTML = "index.html";
-    private final String HTTTP_RESPONSE_GOOD = "HTTP/1.1 200 OK\\r\\n";
-    private final String HTTTP_RESPONSE_NOT_FOUND = "HTTP/1.1 404 Not Found\\r\\n\\r\\n";
-    private final String HTTTP_RESPONSE_INTERNAL_ERROR = "HTTP/1.1 500 Internal Server Error\\r\\n\\r\\n";
 
-    private final String CONTENT_HTML = "Content-type: text/html\\r\\n\\r\\n";
-    private final String CONTENT_JPG = "Content-type: image/jpg\\r\\n\\r\\n";
+    private static final String INDEX_HTML = "index.html";
+    private static final String TEST_JPG = "database.jpg";
+
+    private final String HTTTP_RESPONSE_GOOD = "HTTP/1.1 200 OK\r\n";
+    private final String HTTTP_RESPONSE_NOT_FOUND = "HTTP/1.1 404 Not Found\r\n\r\n";
+    private final String HTTTP_RESPONSE_INTERNAL_ERROR = "HTTP/1.1 500 Internal Server Error\r\n\r\n";
+
+    private final String CONTENT_HTML = "Content-type: text/html\r\n\r\n";
+    private final String CONTENT_JPG = "Content-type: image/jpg\r\n\r\n";
 
     private Socket socket;
+    private BufferedWriter bufferedWriter;
+    private File inputFile;
 
     WebserverConnection(Socket socket){
         this.socket = socket;
@@ -31,9 +33,8 @@ public class WebserverConnection implements Runnable {
         try {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-            while (bufferedReader.readLine() != null){
-                String line = bufferedReader.readLine();
-                if(line.startsWith("GET")){
+            while ((input = bufferedReader.readLine()) != null){
+                if(input.startsWith("GET")){
                     break;
                 }
             }
@@ -42,9 +43,92 @@ public class WebserverConnection implements Runnable {
         }
 
         String fileName = fileNameRegex(input);
+        String header = "";
 
-        
+        if(fileName != null){
+            header = HTTTP_RESPONSE_GOOD;
+        } else {
+            header = HTTTP_RESPONSE_NOT_FOUND;
+        }
 
+        try {
+            inputFile = new File(fileName);
+
+            if(!inputFile.exists()){
+
+                header = HTTTP_RESPONSE_NOT_FOUND;
+                bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+                if(new File("404.html").exists()){
+                    serveNotFound();
+                } else {
+                    bufferedWriter.write(header);
+                    bufferedWriter.flush();
+                    bufferedWriter.close();
+                }
+                return;
+            } else {
+                bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                bufferedWriter.write(HTTTP_RESPONSE_INTERNAL_ERROR);
+            }
+        } catch (IOException e){
+            System.out.println("IOException");
+        }
+
+        try {
+            if(header.equals(HTTTP_RESPONSE_GOOD)){
+                if(fileName.endsWith(".jpg")){
+                    bufferedWriter.write(CONTENT_JPG);
+                    inputFile = new File(fileName);
+                    FileInputStream imageReader = new FileInputStream(inputFile);
+                    byte[] byteArray = new byte[(int)inputFile.length()];
+                    imageReader.read(byteArray);
+                    String output = new String(byteArray, "UTF-8");
+                    bufferedWriter.write(output);
+                    bufferedWriter.flush();
+                    bufferedWriter.close();
+                } else {
+                    //doesn't work with the content type response for some reason
+                    //bufferedWriter.write(CONTENT_HTML);
+                    inputFile = new File(fileName);
+                    FileInputStream fileReader = new FileInputStream(inputFile);
+                    byte[] byteArray = new byte[(int)inputFile.length()];
+                    fileReader.read(byteArray);
+                    String output = new String(byteArray, "UTF-8");
+                    bufferedWriter.write(output);
+                    bufferedWriter.flush();
+                    bufferedWriter.close();
+                }
+            } else {
+
+            }
+        } catch (IOException e){
+            System.out.println("IOException in run()");
+        }
+    }
+
+    public void serveNotFound(){
+        try {
+            BufferedReader notFoundReader = new BufferedReader(new FileReader(new File("404.html")));
+            String output = "";
+            String line = "";
+
+            while ((line = notFoundReader.readLine()) != null){
+                output += line;
+            }
+
+            if(bufferedWriter != null){
+                bufferedWriter.write(HTTTP_RESPONSE_GOOD);
+                bufferedWriter.write(CONTENT_HTML);
+                bufferedWriter.write(output);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+            }
+        } catch (FileNotFoundException e){
+            System.out.println("404 page not found, lol");
+        } catch (IOException e){
+            System.out.println("IOException in serveNotFound");
+        }
     }
 
     public String fileNameRegex(String input) {
@@ -61,10 +145,11 @@ public class WebserverConnection implements Runnable {
                 file = matcher.group(1);
             }
         } catch (Exception e){
-            e.printStackTrace();
+            System.out.println("No file found in request");
+            return null;
         }
 
-        if(file == "/" || file == "") {
+        if(file.equals("/") || file.equals("") || file == null) {
             return INDEX_HTML;
         } else {
             return file;
